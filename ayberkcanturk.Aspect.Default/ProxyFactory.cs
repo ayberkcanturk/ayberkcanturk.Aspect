e0574d048df42df96459fbf36723069b946857a0
ayberk.canturk <ayberk.canturk@gmail.com>
2016-11-24 15:43:04 +0300

    Clean code

--- ayberkcanturk.Aspect.Default/ProxyFactory.cs
+++ ayberkcanturk.Aspect.Default/ProxyFactory.cs
@@ -5,14 +5,15 @@
         public static TI GetTransparentProxy<TI, T>()  where T :TI , new()
         {
             TI instance = new T();
-
             TransparentProxy<TI, T> transparentProxy = new TransparentProxy<TI, T>(instance);
+
             return (TI)transparentProxy.GetTransparentProxy();
         }
 
         public static TI GetTransparentProxy<TI, T>(TI instance) where T : TI, new()
         {
             TransparentProxy<TI, T> transparentProxy = new TransparentProxy<TI, T>(instance);
+
             return (TI)transparentProxy.GetTransparentProxy();
         }
     }
