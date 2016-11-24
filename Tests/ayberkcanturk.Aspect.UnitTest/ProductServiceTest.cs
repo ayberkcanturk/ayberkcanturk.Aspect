b44dcedab994f941dff0497460f9f041c7740508
ayberk.canturk <ayberk.canturk@gmail.com>
2016-11-24 14:37:27 +0300

    ProductServiceTests added.

---
+++ Tests/ayberkcanturk.Aspect.UnitTest/ProductServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ayberkcanturk.Aspect.Default;
+
+namespace ayberkcanturk.Aspect.UnitTest
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        private IProductService productProxyService;
+        private IProductService productService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            productProxyService = ProxyFactory.GetTransparentProxy<IProductService, ProductService>();
+            productService = new ProductService();
+        }
+
+        [TestMethod]
+        public void Is_Proxy_Instance_Of_Type()
+        {
+            Assert.IsInstanceOfType(productProxyService, typeof(IProductService));
+        }
+
+        [TestMethod]
+        public void Is_Intercepted_Method_And_Method_Results_Equal()
+        {
+            Product actual = productService.GetProductWithCache(1);
+            Product expected = productProxyService.GetProduct(1);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
